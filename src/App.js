d33f415ccd8f6a45341844532154acdb10f089e9
Abdo Al Adawy <abdoaladawy2000@gmail.com>
2026-08-04 12:59:49 +0300

    Lesson 6 Making myFirstComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyFirstComponent from "./MyFirstComponent";
 
 function App() {
   return (
@@ -7,8 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to refresh.
         </p>
+
+        <h1>Welcome to My App</h1>
+        <MyFirstComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/MyFirstComponent.js
@@ -0,0 +1,10 @@
+export default function MyFirstComponent() {
+  // Reusable Component
+  return (
+    <div>
+      <h3>Hello, this is my first React component!</h3>
+      <h2>Welcome to my React App!</h2>
+      <button> Click Me</button>
+    </div>
+  );
+}
