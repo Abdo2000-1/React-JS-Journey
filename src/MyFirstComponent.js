0be7d8a2eb5a9f0de3ef4c3eea750aeb2e361678
Abdo Al Adawy <abdoaladawy2000@gmail.com>
2026-08-07 15:51:50 +0300

    CSS File Import In JSX & If Statement into class name

--- src/MyFirstComponent.js
+++ src/MyFirstComponent.js
@@ -1,44 +1,18 @@
+import "./MyFirstComponent.css";
+
 export default function MyFirstComponent() {
   // Reusable Component
-  let s = "Welcome to my React App!"; // { } --> JacaScript Expression
-  let a = 10;
-  let b = 20;
-
   const person = {
-    name: "John",
-    age: 30,
-    city: "New York",
+    name: "Abdo",
+    age: 25,
+    city: "Cairo",
   };
-  // Every thing is Camel Case in React
-  // Style in JSX
-  const elmStyle = {
-    backgroundColor: "lightblue",
-    padding: "10px",
-    borderRadius: "5px",
-  };
+
   return (
-    <div style={elmStyle}>
-      <h3>{s}Hello, this is my first React component!</h3>
-      <h2>{s}</h2>
-      <h2> the first number is {a}</h2>
-      <h2>the second number is {b}</h2>
-      <h2>the sum is {a + b}</h2>
-      <p>{s}This is a simple React component.</p>
-      <button> {s}Click Me</button>
-      <h2>
-        {" "}
-        {person.name} is {person.age} years old and lives in {person.city}.{" "}
-      </h2>
-
-      {/* try Style in JSX */}
-
-      <h1 style={{ color: "blue", fontSize: "24px" }}>Welcome to My App</h1>
-
-      <button onClick={SayHello}>Click Me </button>
-    </div>
+    // Adding Class to The HTML in JSX
+    <h1 className={person.name === "Abdo Al Adawy" ? "Myname" : "Yourname"}>
+      {" "}
+      Abdo Al Adawy{" "}
+    </h1>
   );
-}
-
-function SayHello() {
-  alert("Hello, welcome to my React App!");
 }
